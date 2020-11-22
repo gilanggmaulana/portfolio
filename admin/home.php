06320a0e3735d11c1ad1a19ae5cee761d3b6c52e
gilanggmaulana <gilangmaulanaay@gmail.com>
2020-11-22 12:31:28 +0700

    company
    web company

--- admin/home.php
+++
@@ -1,2 +0,0 @@
-<h2>Selamat Datang Administrator</h2>
-<pre><?php print_r($_SESSION); ?></pre>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-//menghancurkan $_SESSION["pelanggan"]
-session_destroy();
-
-echo "<scrip>alert('anda telah logout');</script>";
-echo "<script>location='index.php';</script>";
-?>
